930f29982d7caf612338bd09bd79ead6fae20f18
MichaelRosado1 <mrosad4@illinois.edu>
2020-11-03 09:32:13 -0600

    Initial Commit

---
+++ binary-tree/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  binary-tree
+//
+//  Created by Michael Rosado on 11/3/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
