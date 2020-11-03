3272106d4d45ba35f68a263d8aeb7768e4fc9969
MichaelRosado1 <mrosad4@illinois.edu>
2020-11-03 09:45:09 -0600

    Added the Node struct.

--- binary-tree/main.cpp
+++ binary-tree/main.cpp
@@ -6,9 +6,24 @@
 //
 
 #include <iostream>
+//Struct for the Node object
+struct Node {
+    //nodes contain value
+    int value;
+    //every node can have a left and right child node, or none at all
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int input) {
+        
+        value = input;
+        //we have to set left and right nodes to null since we don't know if the given node will have child nodes or not
+        left = NULL;
+        right = NULL;
+    }
+};
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
     return 0;
 }
